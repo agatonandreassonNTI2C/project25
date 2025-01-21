f38087364a80ef068039acf46105ecd3daac30b2
agatonandreassonNTI2C <150041085+agatonandreassonNTI2C@users.noreply.github.com>
2025-01-21 16:23:17 +0100

    Begin

--- app.rb
+++ app.rb
@@ -1,5 +1,32 @@
-require 'sinatra'
 require 'slim'
-require 'sqlite3'
+require 'sinatra'
 require 'sinatra/reloader'
-require 'becrypt'
+
+
+
+get ('/') do
+    redirect('/home')
+end
+
+post ('/') do
+    redirect('/home')
+end
+
+get('/home') do
+    slim :home
+end
+
+get('/books') do
+    @book = {title: "Hunger Games", author: 5, price: 25}
+    slim :books
+end
+
+
+get('/cart') do
+    @cart = params[:cart]
+    slim :cart
+end
+
+get('/rate') do
+    slim :rate
+end
