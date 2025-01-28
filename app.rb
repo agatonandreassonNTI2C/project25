294428d5b781aa01288c778eb4bb0d87c6126a91
agatonandreassonNTI2C <150041085+agatonandreassonNTI2C@users.noreply.github.com>
2025-01-28 16:28:53 +0100

    lektion 2

--- app.rb
+++ app.rb
@@ -1,8 +1,6 @@
 require 'slim'
 require 'sinatra'
 require 'sinatra/reloader'
-require 'sinatra'
-require 'slim'
 require 'sqlite3'
 require 'bcrypt'
 
@@ -11,73 +9,33 @@ require 'bcrypt'
 enable :sessions
 
 get('/') do
-  slim(:register)
-end
-
-get('/showlogin') do
-  slim(:login)
-end
-
-post('/login') do
-  username = params[:username]
-  password = params[:password]
-  db = SQLite3::Database.new('db/todo_2021.db')
-  db.results_as_hash = true
-  result = db.execute("SELECT * FROM users WHERE username = ?",username).first
-  pwdigest = result["pwdigest"]
-  id = result["id"]
-
-  if BCrypt::Password.new(pwdigest) == password
-    session[:id] = id
-    redirect('/todos')
-  else
-    "FEL LÖSEN"
-  end
-
-end
-
-get('/todos') do 
-  id = session[:id].to_i
-  db = SQLite3::Database.new('db/todo_2021.db')
-  db.results_as_hash = true
-  result = db.execute("SELECT * FROM todos WHERE user_id = ?",id)
-  slim(:"todos/index",locals:{todos:result})
-end
-
-post('/users/new') do
-  username = params[:username]
-  password = params[:password]
-  password_confirm = params[:password_confirm]
-
-  if (password == password_confirm)
-
-    password_digest = BCrypt::Password.create(password)
-
-
-    db = SQLite3::Database.new('db/user.db')
-
-    db.execute('INSERT INTO user (username,password) VALUES (?,?)',[username,password_digest])
-
-    redirect('/')
-
-  else
-    "Lösenord mathcade inte"
-  end
-end
-
-post ('/') do
-    redirect('/home')
+  slim(:home)
 end
 
 get('/home') do
     slim :home
 end
 
-get('/books') do
-    @book = {title: "Hunger Games", author: 5, price: 25}
-    slim :books
+get('/books') do   
+
+
+    db = SQLite3::Database.new('db/books.db')
+    db.results_as_hash = true
+
+    @result = db.execute("SELECT * FROM books")
+
+    slim (:books)
 end
 
+post('/buy') do
+
+    username = params[:username]
+    password = params[:password]
+    db = SQLite3::Database.new('db/todo_2021.db')
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM users WHERE username = ?",username).first
+    pwdigest = result["pwdigest"]
+    id = result["id"]
 
 get('/cart') do
     @cart = params[:cart]
