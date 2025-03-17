afa61bddb525d8c6ff661f82c0ea60bf83eff29d
agatonandreassonNTI2C <150041085+agatonandreassonNTI2C@users.noreply.github.com>
2025-03-17 11:03:34 +0100

    jobbade

--- app.rb
+++ app.rb
@@ -9,6 +9,7 @@ require 'bcrypt'
 enable :sessions
 db = SQLite3::Database.new('db/library.db')
 db.results_as_hash = true
+id = 0
 
 get('/') do
   slim(:home)
@@ -24,7 +25,22 @@ get('/books') do
     #db = SQLite3::Database.new('db/library.db')
     #db.results_as_hash = true
 
-    @result = db.execute("SELECT * FROM books")
+    @results = db.execute("SELECT * FROM books")
+    @loans = db.execute("SELECT bookid FROM loan WHERE userid = ?", id)
+    
+    @finalresult = {}
+    bookexist = false
+    for result in @results do
+        puts result
+        for loan in @loans do
+            puts loan
+            if result["id"] == loan["bookid"]
+                bookexist = true
+            end
+        end
+
+        @finalresult.merge(@result)
+    end
 
     slim (:books)
 end
@@ -78,14 +94,17 @@ post('/loan') do
 
     username = params[:username]
     password = params[:password]
+    bookid = params[:bookid]
     username = "Agaton.Andreasson@gmail.com"
     userinfo = db.execute("SELECT * FROM users WHERE username = ?",username).first
     #pwdigest = result["pwdigest"]
-    userid = userinfo["id"]
+    puts "bookid"
+    puts bookid
+    puts "userid"
+    puts id
 
-    bookid=2
 
-   if (!userid) 
+   if (id > 0) 
     #    if password==pwdigest
     #        pwd_digest=BCrypt::Password.create(password)
     #        db.execute(”INSERT INTO users(username,pwdigest) VALUES(?,?)”,username,pwdigest)
@@ -93,9 +112,15 @@ post('/loan') do
     #    else
     #        redirect(’/error’) #Lösenord matchar ej
     #    end
-    db.execute("INSERT INTO loan (bookid, userid) VALUES (?, ?)" ,bookid, username).first
+    begin
+        db.execute("INSERT INTO loan (bookid, userid) VALUES (?, ?)", [bookid, id])
+    rescue Exception
+        puts "hej"
+    end
     
    end
+
+   redirect('/books')
 
 end
 
