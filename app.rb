6d0f3759e71faf3e0a2804ff253541a7fc89a0e7
agatonandreassonNTI2C <150041085+agatonandreassonNTI2C@users.noreply.github.com>
2025-05-06 02:09:25 +0200

    Slutliga inlämning

--- app.rb
+++ app.rb
@@ -20,21 +20,21 @@ db.results_as_hash = true
 id = 0
 
 
-# Visar home.slim
+# Shows the home page with a welcome message and a button to go to the different pages
 
 get('/') do
   slim(:home)
 end
 
-# Visar home.slim
+# Shows the home page with a welcome message and a button to go to the different pages
 
 get('/home') do
     slim (:home)
 end
 
-# Visar books.slim och alla böcker i public biblioteket
-#Hämtar böckerna från book tabellen i databasen
-#Skicka localId för att hålla koll på adminanvändaren
+# Shows the books page with all the books in the database
+#Also displays a different view for admin users and normal users.
+
 
 get('/books') do   
 
@@ -52,10 +52,75 @@ get('/books') do
     else
       localId = 0
     end
-    slim :books, locals:{id:localId}
+
+    slim (:"books/index"),locals:{id:localId}
+
 end
 
-#Visar slim sidan där användare kan registrera sig
+
+
+#@param [String] title, The title of the book
+#@param [String] author, The author of the book
+#@param [String] year, The year of the book
+
+#Error message if the title, author or year is empty or if the year is not a number or if the year is not between 0 and 2023 or if the year is not an intager or if the title or author is too long or too short or if the title or author contains multiple spaces or if the author contains numbers.
+
+#@see Model#get_all_books
+
+post ('/addbook') do
+
+  title = params[:title]
+  author = params[:author]
+  year = params[:year]
+  books = get_all_books().last
+  bookid = books["id"]
+  bookid += 1
+
+
+  if title.empty? || author.empty? || year.empty?
+    flash[:index_error] = "Du måste fylla i alla fält."
+    redirect('/books')
+  end
+
+  if year.to_i.to_s != year 
+    flash[:index_error] = "Året får bara innehålla siffror."
+    redirect('/books')
+  end
+
+  if year.to_i < 0 || year.to_i > 2023
+    flash[:index_error] = "Året måste vara mellan 0 och 2023."
+    redirect('/books')
+  end
+
+  if title.length > 40 || author.length > 40
+    flash[:index_error] = "Titeln eller författaren är för lång."
+    redirect('/books')
+  end
+
+  if title.length < 2 || author.length < 4
+    flash[:index_error] = "Titeln eller författaren är för kort."
+    redirect('/books')
+  end
+
+  if author.include?("1") || author.include?("2") || author.include?("3") || author.include?("4") || author.include?("5") || author.include?("6") || author.include?("7") || author.include?("8") || author.include?("9")
+    flash[:index_error] = "Författaren får inte innehålla siffror."
+    redirect('/books')
+  end
+
+  if author.include?("  ") || title.include?("  ")
+    flash[:index_error] = "Titeln eller författaren får inte innehålla flera mellanslag."
+    redirect('/books')
+  end
+
+
+  db.execute("INSERT INTO Books (id, title, author, year) VALUES (?, ?, ?, ?)", [bookid, title, author, year])
+
+
+  redirect('/books')
+
+end
+
+#Post route for registering a new user. Connected to a submit button in the register.slim page.
 
 post('/register') do
     slim(:register)
@@ -65,115 +130,287 @@ end
 #Skapar en ny användare
 #Sparar användarnamn och password_digest i users tabellen i databasen
 
-post('/users/new') do
+#Error message if the username or password is empty or if the login credentials are incorrect. This means that the user is not in the database or that the password is incorrect.
+#Also checks if the username already exists in the database , if the two passwords match and if the password is less than 4 characters long.
+
+#@param [String] username, The username of the user
+#@param [String] password, The password of the user
+#@param [String] password_confirm, The password confirmation of the user
+
+#@see Model#create_user
+#@see Model#select_username
+
+post('/users') do
     username = params[:username]
     password = params[:password]
     password_confirm = params[:password_confirm]
-  
-    if (password == password_confirm)
-  
-      password_digest = BCrypt::Password.create(password)
-      create_user(username,password_digest)
-      redirect('/')
-  
+
+
+    users = select_username(username)
+    
+
+
+    if username.empty? || password.empty? || password_confirm.empty?
+        flash[:login_error] = "Du måste fylla i alla fält."
+        redirect('/showlogin')
+    end
+
+    if password.length < 4
+      flash[:login_error] = "Lösenordet måste vara minst 4 tecken långt."
+      redirect('/showlogin')
+    end
+    
+    if users.include?(username) 
+        flash[:login_error] = "Användarnamnet existerar redan."
+        redirect('/showlogin')
     else
-      redirect('/')
+      if (password == password_confirm)
+    
+        password_digest = BCrypt::Password.create(password)
+        create_user(username,password_digest)
+        redirect('/')
+    
+      else
+        flash[:login_error] = "Du måste ha samma lösenord."
+        redirect('/showlogin')
+      end
     end
 end
 
-#Visar inloggningssidan
+#Shows the login page for the user to login, logout or register
   
 get('/showlogin') do
     slim(:login)
 end
 
-#Hanterar logik för om anävndare kan logga in
-#Jämför lösenord från users tabellen i databasen
+#Post route for logging in a user. Checks the username and password against the database and if they match, it sets the session id and username.
+
+
+#Error message if the username or password is empty or if the login credentials are incorrect. This means that the user is not in the database or that the password is incorrect.
+
+
+#@param [String] username, The username of the user
+#@param [String] password, The password of the user
+
+#@see Model#get_user_by_username
   
 post('/login') do
     username = params[:username]
     password = params[:password]
+
+    if username.empty? || password.empty?
+      flash[:login_error] = "Du måste fylla i alla fält."
+      redirect('/showlogin')
+    end
+
     user = get_user_by_username(username)
     pwdigest = user["pwdigest"]
     id = user["id"]
 
-    
-  
+
     if BCrypt::Password.new(pwdigest) == password
       session[:id] = id
       session[:username] = username
-      redirect('/home')
+      redirect('/showlogin')
     else
-      flash[:password_error] = "Fel Lösenord."
+      flash[:login_error] = "Fel Inloggningsuppgifter."
+      redirect('/showlogin')
     end
   
 end
 
-#Logik för användare som trycker på knappen för att låna böcker
-#Tar information från users och books tabellerna till loan tabellen.
-#
+#Post route for logging out a user
+
+#Checks if the user is logged in and if not, redirects to the login page with an error message
+#If the user is logged in, it clears the session and redirects to the login page with a logout message
+
+#Error message if the user is not logged in, or if the user is already logged out and trying to push the logout button again.
+
+
+post('/logout') do
+
+  if session[:id] == nil
+    flash[:login_error] = "Du är inte inloggad."
+    redirect('/showlogin')
+  else
+
+    session[:id] = nil
+    session[:username] = nil
+    flash[:login_error] = "Du har loggat ut."
+    redirect('/showlogin')
+  end
+
+end
+
+#Post route for loaning a book. Connected to a submit button in the books.slim page. 
+
+#Error message if the user is not logged in or if the book is already loaned by the same user
+
+#@param [String] bookid, The id of the book to be loaned
+#@param [String] username, The username of the user who is loaning the book	
+#@param [String] password, The password of the user who is loaning the book
+
+#@see Model#create_user_book
+#@see Model#select_bookid
+#@see Model#get_user_by_username
 
 post('/loan') do
+  owners = select_bookid(session[:id])
+  book_ids = owners.map { |book| book["bookid"] }
+  value = params[:bookid].to_i
+
+  id = session[:id]
+  puts id
+  puts (id == nil)
+
+  if (id == nil)
+    flash[:index_error] = "Du måste vara inloggad för att låna böcker."
+    redirect('/books')
+  end
+
+
+  if !book_ids.include?(value)
+
 
     username = params[:username]
     password = params[:password]
     bookid = params[:bookid]
-    username = "Agaton"
     userinfo = get_user_by_username(username)
-    id = session[:id]
-    puts "bookid"
-    puts bookid
-    puts "userid"
-    puts id
 
 
-   if (id > 0) 
-    create_user_book(bookid, id)    
-   end
+    if (id > 0) 
+      create_user_book(bookid, id)    
+    end
 
-   redirect('/books')
+    redirect('/books')
+
+  else
+    flash[:index_error] = "Du har redan lånat boken."
+    redirect('/books')
+  end
 
 end
 
-#Visar användares personliga bibliotek
-#Hanterar lånade böcker från loan tabellen och skriver ut följande böcker från book tabellen
+
+#Post route for editing a book in the admin view
+#Checks if the user is logged in and if not, redirects to the login page with an error message
+#If the user is logged in, it updates the book title in the database and redirects to the books page
+
+#Error message if the title is empty or too long or if it contains multiple spaces, or if it is too long. 
+
+
+#@param [String] bookid, The id of the book to be edited
+#@param [String] title, The new title of the book
+
+#@see Model#update_book_title
+
+post('/adminedit') do
+
+  puts "admin edit"
+  puts params[:bookid]
+  puts params[:title]
+
+  if params[:title].empty?
+    flash[:index_error] = "Du måste fylla i alla fält."
+    redirect('/books')
+  end
+
+  if params[:title].length > 40
+    flash[:index_error] = "Titeln är för lång."
+    redirect('/books')
+  end
+
+  if params[:title].include?("  ")
+    flash[:index_error] = "Titeln får inte innehålla flera mellanslag." 
+    redirect('/books')
+  end
+
+  
+  update_book_title(params[:title], params[:bookid])
+
+
+  redirect('/books')
+
+end
+
+
+
+
+
+#Checks if the user is logged in before accessing the library page
+
+#Error message if the user is not logged in
+
+before ('/library') do
+  if session[:username] == nil
+    flash[:home_error] = "Du måste vara inloggad för att kunna se dina böcker."
+    redirect('/home')
+  end
+end
+
+
+#Get route for the library page
+#Shows all the books that different users have loaned.
+
+#see Model#get_all_loans_with_users
 
 get('/library') do
-
-    
-
-    db = SQLite3::Database.new('db/library.db')
-    db.results_as_hash = true
-
     name = session[:username]
-
-    userloans = get_user_books(session[:id]) 
-    puts userloans
-
-    users = db.execute("SELECT * FROM users")
-    slim :library, locals:{users:users, userloans:userloans}
+    currentUserId = session[:id]
+    userloans = get_all_loans_with_users()
+    slim :library, locals:{userloans:userloans, currentUserId:currentUserId}
 end
 
-#Logik för en delete knapp i användares personliga bibliotek. 
-#Tar bort rader från loan tabellen
+#Creates all users and opens the slimp page users.slim
+
+
+get('/users') do
+
+  users = get_all_users()
+  slim :users, locals:{users:users}
+
+end
+
+#Post route for deleting a book from the user's library
+#Checks if the user is logged in to the correct acount, and if not, redirects to the login page with an error message
+#If the user is logged in, it deletes the book from the user's library and redirects to the library page
+
+
+#@param [String] bookId, The id of the book to be deleted
+
+#@see Model#delete_user_book
+#@see Model#select_userid
 
 post('/delete') do
 
-    puts "userid"
-    puts "#{session[:id]}"
-    puts "bookid"
-    puts "#{params[:bookId]}"
+  bookId = params[:bookId].to_i
+
+  userId = session[:id]
+
+  owners = select_userid(bookId)
+
+  puts "bookId param (to_i): #{bookId}"
+  puts "userId from session: #{userId}"
 
 
-    delete_user_book(session[:id], params[:bookId])  
+
+  if owners.any?{ |hash| hash["userid"] == userId } 
+    delete_user_book(userId, bookId)  
     redirect('/library')
+  else
+    flash[:library_error] = "Du äger inte denna boken."
+    redirect('/library')
+  end
 end
 
-#Skapar en delete knapp för admin användaren i public biblioteket
-#Tar bort böcker från book tabellen
 
+#Post route for deleting a book from the database. Only accessible by admin users.
 
-post('/books/delete') do
+#@param [String] bookid, The id of the book to be deleted
+
+#@see Model#delete_book
+
+post('/bookdelete') do
 
   puts "userid"
   puts "#{session[:id]}"
@@ -184,5 +421,42 @@ post('/books/delete') do
 
 
   redirect('/books')
+
+end
+
+#Post route for deleting a user from the database. Only accessible by admin users.
+#Checks if the user is logged in and if not, redirects to the login page with an error message
+#If the user is logged in, it deletes the user from the database and redirects to the users page
+
+#Checks if the user is an admin and if not, redirects to the users page with an error message
+#If the user is an admin and tries to delete themselves, redirects to the users page with an error message
+
+#@param [String] bookid, The id of the book to be deleted
+
+#@param [String] userid, The id of the user to be deleted
+
+#@see Model#delete_user
+
+
+post ('/userdelete') do
+  puts "userid"
+  puts "#{params[:userid]}"
+
+  if session[:id] == 11
+
+    userid = params[:userid].to_i
+
+    if session[:id] != userid
+
+      delete_user(userid)
+      redirect('/users')
+    else
+      flash[:user_error] = "Du kan inte ta bort dig själv."
+      redirect('/users')
+    end
+  else
+    flash[:user_error] = "Du är inte admin."
+    redirect('/users')
+  end
 
 end
--- model/model.rb
+++ model/model.rb
@@ -1,6 +1,7 @@
 require 'sqlite3'
 
-#Skapar databasen
+#Creates a new SQLite3 database connection and sets the results to be returned as hashes
+#This is a helper method to avoid repeating the same code in multiple places
 
 def get_db()
     db = SQLite3::Database.new('db/library.db')
@@ -8,51 +9,104 @@ def get_db()
     return db
 end
 
-#Räknar upp alla böcker från book tabellen
+#Selects usernames from the users table and returns them as an array of hashes
+
+def select_username(username)
+    db = get_db()
+    db.execute("SELECT username FROM users").map { |row| row["username"] } 
+end
+
+#Selects userid from the loan table according to the bookid
+
+def select_userid(bookId)
+    db = get_db()
+    db.execute("SELECT userid FROM loan WHERE bookid=?", bookId)
+end
+
+#Selects bookid from the loan table according to the userid
+
+def select_bookid(id)
+    db = get_db()
+    db.execute("SELECT bookid FROM loan WHERE userid=?",id)
+end
+
+#Selects all users from the users table and returns them as an array of hashes. Returns all columns from the users table
+
+def get_all_users()
+    db = get_db()
+    db.execute("SELECT * FROM Users")
+end
+
+#Selects title, author, year, and id from the books table and select, username and id from the users table. The it joins the two tables and returns those variables.
+
+def get_all_loans_with_users
+    db = get_db()
+    db.execute("SELECT Books.title, Books.author, Books.year, Books.id AS bookid, Users.username, Users.id AS userid FROM Loan 
+    JOIN Books ON Loan.bookid = Books.id JOIN Users ON Loan.userid = Users.id")
+end
+  
+
+#Selects all books from the books table and returns them as an array of hashes. Returns all columns from the books table
 
 def get_all_books()
     db = get_db()
     return db.execute("SELECT * FROM books")
 end
 
-#Skapar användare i user tabellen
+#creates a new user according to the username and password digest. The password digest is a hashed version of the password.
 
 def create_user(username,password_digest)
     db = get_db()
     db.execute('INSERT INTO users (username,pwdigest) VALUES (?,?)',[username,password_digest])
 end
 
-#Skapar specifika användare
+#Updates a book title i the Books table according to the bookid. The bookid is the id of the book that is being updated.
+
+def update_book_title(title, bookid)
+
+    db = get_db()
+
+    db.execute("UPDATE Books SET title=? Where id =?", [title, bookid])
+
+end
+
+#Selects * from user table from a specific username.
 
 def get_user_by_username(username)
     db = get_db()
     return db.execute("SELECT * FROM users WHERE username = ?",username).first
 end
 
-#Lägger till böcker i loan tabellen 
+#LCreates a user book which inserts the bookid and userid into the loan table. The loan table is a joint table between the users and books tables.
 
 def create_user_book(bookid, id)
     db = get_db()
     db.execute("INSERT INTO loan (bookid, userid) VALUES (?, ?)", [bookid, id])
 end
 
-#Räknar upp användares böcker
+#Selects * from books that the owners of the books have loaned. It selects i from the books table with regard to the loan table.
 
 def get_user_books(id)
     db = get_db()
     return db.execute("SELECT * FROM books WHERE id IN (SELECT bookid FROM loan WHERE userid = ?)", id)
 end
 
-#Tar bort användres böcker från loan tabellen
+#Removes a book from the loan table according to bookid and userid.
 
 def delete_user_book(id, bookid)
     db = get_db()
     db.execute("DELETE FROM loan WHERE userid = ? and bookid = ?", [id, bookid])
 end
 
-#Tar bort böcker från public biblioteket
-
+#Removes a book from the books table according to bookid. The bookid is the id of the book that is being deleted.
 def delete_book(bookid)
     db = get_db()
     db.execute("DELETE FROM Books WHERE id = ?", [bookid]) 
+end
+
+#Deletes a user and removes it from the users table according to the id. The id is the id of the user that is being deleted.
+
+def delete_user(id)
+    db = get_db()
+    db.execute("DELETE FROM Users WHERE id = ?", [id]) 
 end
